f0a8da0dff2dfa643efe588d6219d11e8a3cda89
Mike Wiesenhart <mike@mdwbkny.com>
2015-12-14 17:14:24 -0500

    Adding Devise and customising the field and stuff

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,18 @@
+module DeviseHelper
+	def devise_error_messages!
+		return '' if resource.errors.empty?
+
+		messages = resource.errors.messages.map { |attr,msg| content_tag(:li, msg.join(', ')) }.join
+		sentence = Il8n.t('errors.messages.not_saved', count: resource.errors.count, resources: resource.class.model_name.human.downcase)
+
+		html = <<-HTML
+		<div class="alert alert-danger alert-dismissable">
+			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+			<h4>#{sentence}</h4>
+			#{messages}
+		</div>
+		HTML
+
+		html.html_safe
+	end
+end
