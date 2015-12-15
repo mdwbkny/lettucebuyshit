cb658a8d2dff3423f5d5c72d4cd24260c2eda0e3
Mike Wiesenhart <mike@mdwbkny.com>
2015-12-14 19:45:30 -0500

    Fixed issue with Devise Helper

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,8 +2,8 @@ module DeviseHelper
 	def devise_error_messages!
 		return '' if resource.errors.empty?
 
-		messages = resource.errors.messages.map { |attr,msg| content_tag(:li, msg.join(', ')) }.join
-		sentence = I18n.t('errors.messages.not_saved', count: resource.errors.count, resources: resource.class.model_name.human.downcase)
+		messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+		sentence = I18n.t('errors.messages.not_saved', count: resource.errors.count, resource: resource.class.model_name.human.downcase)
 
 		html = <<-HTML
 		<div class="alert alert-danger alert-dismissable">
