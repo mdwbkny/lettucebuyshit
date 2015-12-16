f70a7e8cfef29913b1f22c1a0fafe679ed18d91f
Mike Wiesenhart <mike@mdwbkny.com>
2015-12-16 10:20:49 -0500

    Created an order page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,5 +6,6 @@ class Listing < ActiveRecord::Base
 
     validates :name, :description, :price, presence: true
 
-    belongs_to :user												
+    belongs_to :user
+    has_many :orders												
 end
